93ca2665586fbf040c9f6e94118c4ce5f37178dd
LemonCode857 <786986461@qq.com>
2021-08-20 17:05:52 +0800

    20210820-01

--- Algorithm/src/zuoshen/SelectSort.java
+++ Algorithm/src/zuoshen/SelectSort.java
@@ -1,8 +1,31 @@
 package zuoshen;
 
+import java.util.*;
+
 /**
  * 选择排序
  * @author 78698
  */
 public class SelectSort {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+    }
+
+    public static void getSort(int[] ary){
+        // 如果数组为空或其长度小于2那还排个88
+        if (ary == null ||ary.length < 2){
+            return;
+        }
+        // i 到 N-1
+        for (int i = 0; i < ary.length - 1; i++){
+            int min = i;
+            for (int y = i; y < ary.length - 1; y++){
+                // i 到 N-1 上获得最小值的下标
+
+            }
+
+        }
+    }
 }
